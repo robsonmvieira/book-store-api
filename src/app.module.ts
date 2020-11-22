864bd3b2d1fb8451de9757e745b72461f38abced
Robson Maia <robsonmvieira@gmail.com>
2020-11-22 12:13:40 -0300

    :sparkles: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './category/category.module';
+import { PublisherModule } from './publisher/publisher.module';
+import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), CategoryModule],
+  imports: [TypeOrmModule.forRoot(), CategoryModule, PublisherModule, AuthorsModule, BooksModule],
   controllers: [],
   providers: [],
 })

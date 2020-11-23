9f3eb2ff4bd17b3cdf7e785fa11de59d18b04657
Robson Maia <robsonmvieira@gmail.com>
2020-11-23 08:22:41 -0300

    :sparkles: segragated common properties to base

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,18 +1,10 @@
+import { Base } from 'src/crosscutting/base.entity';
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('categories')
-export default class Category {
-
-  @PrimaryGeneratedColumn("uuid")
-  id: string
+export default class Category extends Base {
 
   @Column()
   name: string
-
-  @CreateDateColumn({ type: 'timestamp with time zone' })
-  created_at: Date
-
-  @UpdateDateColumn({ type: 'timestamp with time zone' })
-  updated_at: Date
 
 }

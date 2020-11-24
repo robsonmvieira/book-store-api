3ac5ec38be846c498a4cefb4713cb536f1c3da56
Robson Maia <robsonmvieira@gmail.com>
2020-11-24 12:56:04 -0300

    :sparkles: update create publisher DTO

--- src/modules/publisher/dto/create-publisher.dto.ts
+++ src/modules/publisher/dto/create-publisher.dto.ts
@@ -1,1 +1,22 @@
-export class CreatePublisherDto {}
+import { Author } from './../../authors/entities/author.entity';
+import { Book } from './../../books/entities/book.entity';
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+
+export class CreatePublisherDto {
+  @IsNotEmpty({message: 'Você precisa informar um nome para a editora'})
+  name: string
+
+  @IsString({message: 'O Tipo de dado não aceito. Aceito apenas caracteres Alpha'})
+  @IsNotEmpty({message: 'Esse campo não pode estar vazio'})
+  web_site: string
+
+  @IsEmail()
+  @IsNotEmpty({message: 'Esse campo não pode estar vazio'})
+  email: string
+
+
+  books?: Book[]
+
+
+  authors?: Author[]
+}

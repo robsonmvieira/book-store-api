d54ef85e93aa38004b184505f793f1c60a571435
Robson Maia <robsonmvieira@gmail.com>
2020-11-28 18:39:48 -0300

    :recycle: setup  inheritance in category service

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import  CategoryService  from './category.service';
 import  CreateCategoryDto  from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Controller('categories')
 export default class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
+  async create(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Category[]> {
     return this.categoryService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.categoryService.findOne(id);
+  async findOne(@Param('id') id: number): Promise<Category> {
+    return this.categoryService.findOne(String(id));
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(id, updateCategoryDto);
+  async update(@Param('id') id: number, @Body() updateCategoryDto: UpdateCategoryDto): Promise<Category> {
+    return this.categoryService.update(String(id), updateCategoryDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.categoryService.remove(id);
+  async remove(@Param('id') id: number):Promise<boolean>{
+    return this.categoryService.remove(String(id));
   }
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,58 +1,13 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import  CreateCategoryDto  from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { BaseService } from '../../infra/BaseService.service'
 import { Category } from './entities/category.entity'
 @Injectable()
-export default class CategoryService {
+export default class CategoryService extends BaseService<Category> {
 
-  constructor(@InjectRepository(Category) private resource: Repository<Category>) {}
-
-  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
-     const newCategory = await this.resource.save(createCategoryDto)
-
-     if (!newCategory) {
-       throw new InternalServerErrorException('ocorreu um erro interno. Por favor tente mais tarde')
-     }
-
-     return newCategory
-  }
-
-  async findAll(): Promise<Category[]> {
-    return await this.resource.find();
-  }
-
-  async findOne(id: number): Promise<Category> {
-    const category = await this.resource.findOne(String(id))
-
-    if(!category) {
-      throw new  NotFoundException("Category not Found")
-    }
-    return category
-  }
-
-  async update(id: number, updateCategoryDto: UpdateCategoryDto): Promise<Category> {
-    const category = await this.resource.findOne(String(id))
-
-    if(!category) {
-      throw new NotFoundException('Category Not Found')
-    }
-
-    await this.resource.update(id, updateCategoryDto)
-
-    const updatedCategory = Object.assign({...category, ...updateCategoryDto})
-
-    return updatedCategory
-  }
-
-  async remove(id: number): Promise<boolean> {
-
-    const result  = await this.resource.delete(String(id))
-
-    if(!result) {
-      throw new NotFoundException('category not found')
-    }
-    return true
+  constructor(@InjectRepository(Category) resource: Repository<Category>) {
+    super(resource)
   }
 }

b50089acb1d650a41c7c3dc179dcf94c47dacd0b
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 18:01:02 -0300

    :sparkles: add roles services

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,26 +1,12 @@
+import {BaseService} from '../../infra/BaseService.service'
 import { Injectable } from '@nestjs/common';
-import { CreateRoleDto } from './dto/create-role.dto';
-import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './entities/role.entity';
 
 @Injectable()
-export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
-  }
-
-  findAll() {
-    return `This action returns all roles`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
-  }
-
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+export class RolesService extends BaseService<Role> {
+  constructor(@InjectRepository(Role) resource: Repository<Role>) {
+    super(resource)
   }
 }

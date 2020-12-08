6cec37ced223318e5648846b5465593b4dace26b
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 16:04:01 -0300

    :sparkles: add property for permission model

--- src/modules/permissions/entities/permission.entity.ts
+++ src/modules/permissions/entities/permission.entity.ts
@@ -1,1 +1,22 @@
-export class Permission {}
+import { Role } from '@modules/roles/entities/role.entity';
+import { Column, Entity, JoinTable, ManyToMany } from 'typeorm';
+import { Base } from '../../../crosscutting/base.entity'
+
+@Entity('permissions')
+export class Permission extends Base {
+
+  @Column()
+  name: string
+
+  @Column()
+  description?: string
+
+  @Column()
+  slug?: string
+
+  @ManyToMany(() => Role)
+  @JoinTable()
+  roles: Role[]
+
+
+}

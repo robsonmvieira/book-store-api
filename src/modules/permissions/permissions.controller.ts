03f8d7c56562a8377f97ccc0a469821367e15cc3
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 18:08:59 -0300

    :recycle: refactor permission controller

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { Permission } from './entities/permission.entity';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Post()
-  create(@Body() createPermissionDto: CreatePermissionDto) {
+  async create(@Body() createPermissionDto: CreatePermissionDto): Promise<Permission> {
     return this.permissionsService.create(createPermissionDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Permission[]> {
     return this.permissionsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Permission> {
+    return this.permissionsService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  async update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto): Promise<Permission> {
+    return this.permissionsService.update(id, updatePermissionDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+  async remove(@Param('id') id: string): Promise<boolean> {
+    return this.permissionsService.remove(id);
   }
 }

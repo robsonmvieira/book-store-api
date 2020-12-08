920df5faef1c7f089049509b6ea8f2ae402e01ff
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 16:04:09 -0300

    :sparkles: add property for role model

--- src/modules/roles/entities/role.entity.ts
+++ src/modules/roles/entities/role.entity.ts
@@ -1,1 +1,26 @@
-export class Role {}
+import { User } from './../../users/entities/user.entity';
+import { Permission } from './../../permissions/entities/permission.entity';
+
+import { Column, Entity, JoinTable, ManyToMany } from 'typeorm';
+import { Base } from '../../../crosscutting/base.entity'
+
+@Entity('roles')
+export class Role extends Base {
+
+  @Column()
+  name: string
+
+  @Column()
+  description?: string
+
+  @Column()
+  slug?: string
+  @ManyToMany(() => Permission)
+  @JoinTable()
+  permissions: Permission[]
+
+  @ManyToMany(() => User)
+  @JoinTable()
+  users: User[]
+
+}

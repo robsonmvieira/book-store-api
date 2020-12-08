bb70e72e8ce0b32a4c5c3301e711e5eeab1bf46b
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 16:15:55 -0300

    :sparkles: add property for updateUserDTO

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,39 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, MinLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+  @ApiProperty()
+  @IsString()
+  name?: string
+
+  @ApiProperty()
+  @IsString()
+  @IsEmail()
+  email: string
+
+  @ApiProperty()
+  @IsString()
+  class?: string
+
+  @ApiProperty()
+  semester?: number
+
+  @ApiProperty()
+  @MinLength(6)
+  password?: string
+
+  @ApiProperty()
+  @IsString()
+  course?: string
+
+  @IsString()
+  @ApiProperty()
+  profile?: string
+
+  @IsString()
+  @ApiProperty()
+  role?: string[]
+}

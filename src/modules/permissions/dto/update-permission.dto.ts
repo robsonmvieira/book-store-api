de24e67626cc27b51deeb8a2799887eb551d76ad
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 16:26:05 -0300

    :sparkles: add property for updatePermissionDTO

--- src/modules/permissions/dto/update-permission.dto.ts
+++ src/modules/permissions/dto/update-permission.dto.ts
@@ -1,4 +1,22 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { CreatePermissionDto } from './create-permission.dto';
 
-export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {}
+export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {
+
+  @ApiProperty()
+  @IsString()
+  name?: string
+
+  @ApiProperty()
+  @IsString()
+  description?: string
+
+  @ApiProperty()
+  slug?: string
+
+  @ApiProperty()
+  roles?: string[]
+
+}

40f5af4ee6f4098ca2cfbd0998f359a397757e69
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 16:23:18 -0300

    :sparkles: add property for updateRoleDTO

--- src/modules/roles/dto/update-role.dto.ts
+++ src/modules/roles/dto/update-role.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { CreateRoleDto } from './create-role.dto';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {
+
+  @ApiProperty()
+  @IsString()
+  name?: string
+
+  @ApiProperty()
+  @IsString()
+  description?: string
+
+  @ApiProperty()
+  slug?: string
+
+  @ApiProperty()
+  permissions?: string[]
+
+  @ApiProperty()
+  users?: string[]
+}

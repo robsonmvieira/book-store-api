39b1277b833e806e651d709b05d887d229b85585
Robson Maia <robsonmvieira@gmail.com>
2020-12-08 16:25:55 -0300

    :sparkles: add property for createPermissionDTO

--- src/modules/permissions/dto/create-permission.dto.ts
+++ src/modules/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePermissionDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsString, IsNotEmpty } from "class-validator"
+
+export class CreatePermissionDto {
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty({ message: 'O nome da permissão é obrigatório' })
+  name: string
+
+  @ApiProperty()
+  @IsString()
+  description?: string
+
+  @ApiProperty()
+  slug?: string
+
+  @ApiProperty()
+  roles?: string[]
+}

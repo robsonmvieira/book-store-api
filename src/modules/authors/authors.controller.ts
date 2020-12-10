fb33dc780b4cba1016bcd0ee72ef991f8f30c82b
Robson Maia <robsonmvieira@gmail.com>
2020-12-10 12:00:33 -0300

    :recycle: setup author controller

--- src/modules/authors/authors.controller.ts
+++ src/modules/authors/authors.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { Author } from './entities/author.entity';
 
 @Controller('authors')
 export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
   @Post()
-  create(@Body() createAuthorDto: CreateAuthorDto) {
+  async create(@Body() createAuthorDto: CreateAuthorDto): Promise<Author> {
     return this.authorsService.create(createAuthorDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Author[]> {
     return this.authorsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authorsService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Author> {
+    return this.authorsService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
-    return this.authorsService.update(+id, updateAuthorDto);
+  async update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto): Promise<Author> {
+    return this.authorsService.update(id, updateAuthorDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authorsService.remove(+id);
+  async remove(@Param('id') id: string): Promise<boolean> {
+    return this.authorsService.remove(id);
   }
 }

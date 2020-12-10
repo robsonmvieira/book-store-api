199ebba5e37e665ddf77f4686abc7aa80886c7dd
Robson Maia <robsonmvieira@gmail.com>
2020-12-10 12:02:01 -0300

    :building_construction: inject dep in author module

--- src/modules/authors/authors.module.ts
+++ src/modules/authors/authors.module.ts
@@ -3,9 +3,10 @@ import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from './entities/author.entity';
+import { Publisher } from '../publisher/entities/publisher.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Author])],
+  imports: [TypeOrmModule.forFeature([Author, Publisher])],
   controllers: [AuthorsController],
   providers: [AuthorsService]
 })

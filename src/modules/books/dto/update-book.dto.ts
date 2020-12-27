f93c977663f55ca5af5c9a883787f4ede91dfa96
Robson Maia <robsonmvieira@gmail.com>
2020-12-27 20:38:05 -0300

    :sparkles: add update book dto property

--- src/modules/books/dto/update-book.dto.ts
+++ src/modules/books/dto/update-book.dto.ts
@@ -1,4 +1,29 @@
+import { Author } from '@modules/authors/entities/author.entity';
+import { Publisher } from '@modules/publisher/entities/publisher.entity';
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateBookDto } from './create-book.dto';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+
+  @ApiProperty()
+  name?: string
+
+  @ApiProperty()
+  pages?: number
+
+  @ApiProperty()
+  ISBN_10?: string
+
+  @ApiProperty()
+  ISBN_13?: string
+
+  @ApiProperty()
+  publisher_id?: string
+
+  @ApiProperty()
+  publisher?: Publisher
+
+  @ApiProperty()
+  authors?: Author[]
+}

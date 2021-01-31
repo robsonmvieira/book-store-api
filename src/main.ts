cc9bf24c64aaab6fc7fd068bc827c495242679f7
Robson Maia <robsonmvieira@gmail.com>
2021-01-31 17:35:38 -0300

    :recycle: up application validator

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    forbidNonWhitelisted: true,
+    whitelist: true,
+    transform: true
+  }));
   app.enableCors()
 
   const options = new DocumentBuilder()
@@ -14,6 +18,7 @@ async function bootstrap() {
     .setDescription('This is APi to book Store')
     .setVersion('1.0')
     .addTag('books')
+    .addTag('authors')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);

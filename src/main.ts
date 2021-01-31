5a6978d047888d6a5b023b5909af5207379fe263
Robson Maia <robsonmvieira@gmail.com>
2021-01-31 20:08:26 -0300

    :sparkles: turn on file upload

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
-import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
+// import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+// import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,15 +13,15 @@ async function bootstrap() {
   }));
   app.enableCors()
 
-  const options = new DocumentBuilder()
-    .setTitle('Book Api')
-    .setDescription('This is APi to book Store')
-    .setVersion('1.0')
-    .addTag('books')
-    .addTag('authors')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  // const options = new DocumentBuilder()
+  //   .setTitle('Book Api')
+  //   .setDescription('This is APi to book Store')
+  //   .setVersion('1.0')
+  //   .addTag('books')
+  //   .addTag('authors')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api', app, document);
   await app.listen(8000);
 }
 bootstrap();
--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { diskStorage } from 'multer'
+import { FileInterceptor } from '@nestjs/platform-express';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './entities/book.entity';
+import { extname } from 'path'
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
-  @Post()
-  create(@Body() createBookDto: CreateBookDto) {
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', { storage: diskStorage({
+    destination: './uploads',
+    filename: (req, file, cb) => {
+      const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+      cb(null, `${randomName}${extname(file.originalname)}`)
+    }
+  }) }))
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async create(@UploadedFile() file, @Body() createBookDto: CreateBookDto): Promise<Book> {
+
+    createBookDto.image = file.filename
+    console.log(file)
     return this.booksService.create(createBookDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Book[]> {
     return this.booksService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.booksService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Book> {
+    return this.booksService.findOne(id);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(+id, updateBookDto);
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto): Promise<Book> {
+    return this.booksService.update(id, updateBookDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.booksService.remove(+id);
+  async remove(@Param('id') id: string): Promise<boolean> {
+    return this.booksService.remove(id);
   }
 }

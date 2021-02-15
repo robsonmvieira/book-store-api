85b8bb920b6e3ab64576092476c4eb4b406cb2a7
Robson Maia <robsonmvieira@gmail.com>
2021-02-15 09:53:13 -0300

    :recycle: fix http method decorator

--- src/modules/category/use-cases/delete-category/delete-category-by-id.http.controller.ts
+++ src/modules/category/use-cases/delete-category/delete-category-by-id.http.controller.ts
@@ -1,13 +1,13 @@
 import { routes } from "@infra/configs/app.routes";
 import CategoryService from "@modules/category/category.service";
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Delete, Param } from "@nestjs/common";
 
 @Controller()
 export class DeleteCategoryByIdHttpController {
 
   constructor(private readonly categoryService: CategoryService) {}
 
-  @Get(routes.category.remove)
+  @Delete(routes.category.remove)
   async remove(@Param('id') id: string):Promise<boolean>{
     return this.categoryService.remove(id);
   }

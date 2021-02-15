79a17f438b47406db5f5dbfaa172f2f372bcab35
Robson Maia <robsonmvieira@gmail.com>
2021-02-15 09:41:32 -0300

    :sparkles: create category use case

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -6,6 +6,7 @@ import  { Category }  from './entities/category.entity';
 import { FindCategoriesHttpController } from './use-cases/find-categories/find-categories.http.controller';
 import { FindCategoryByIdHttpController } from './use-cases/find-category-by-id/find-category-by-id.http.controller';
 import { DeleteCategoryByIdHttpController } from './use-cases/delete-category/delete-category-by-id.http.controller';
+import { CreateCategoryHttpController } from './use-cases/create-category/create-category.http';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { DeleteCategoryByIdHttpController } from './use-cases/delete-category/de
     CategoryController,
     FindCategoriesHttpController,
     FindCategoryByIdHttpController,
-    DeleteCategoryByIdHttpController
+    DeleteCategoryByIdHttpController,
+    CreateCategoryHttpController
   ],
   providers: [CategoryService]
 })
---
+++ src/modules/category/use-cases/create-category/create-category.http.ts
@@ -0,0 +1,16 @@
+import { routes } from "@infra/configs/app.routes";
+import CategoryService from "@modules/category/category.service";
+import CreateCategoryDto from "@modules/category/dtos/create-category.dto";
+import { Category } from "@modules/category/entities/category.entity";
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller()
+export class CreateCategoryHttpController {
+
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post(routes.category.root)
+  async create(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
+    return this.categoryService.create(createCategoryDto);
+  }
+}

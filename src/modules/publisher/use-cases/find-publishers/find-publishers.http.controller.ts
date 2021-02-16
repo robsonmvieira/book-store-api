f5a6bf2c3905ee9f2789fe4ad34b00025f725e3d
Robson Maia <robsonmvieira@gmail.com>
2021-02-16 16:16:55 -0300

    :sparkles: add find all publisher add case

---
+++ src/modules/publisher/use-cases/find-publishers/find-publishers.http.controller.ts
@@ -0,0 +1,15 @@
+import { routes } from "@infra/configs/app.routes";
+import { Publisher } from "@modules/publisher/entities/publisher.entity";
+import { PublisherService } from "@modules/publisher/publisher.service";
+import { Controller, Get } from "@nestjs/common";
+
+@Controller()
+export class FindPublisherHttpController {
+
+  constructor(private readonly publisherService: PublisherService) {}
+
+  @Get(routes.publisher.root)
+  async findAll(): Promise<Publisher[]> {
+    return this.publisherService.findAll();
+  }
+}

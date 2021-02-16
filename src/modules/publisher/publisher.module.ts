57f6fac57d1697a226f1f4a68031d207df25b818
Robson Maia <robsonmvieira@gmail.com>
2021-02-16 16:17:27 -0300

    :wrench: update publisher module

--- src/modules/publisher/publisher.module.ts
+++ src/modules/publisher/publisher.module.ts
@@ -3,10 +3,14 @@ import { PublisherService } from './publisher.service';
 import { PublisherController } from './publisher.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Publisher } from './entities/publisher.entity';
+import { FindPublisherHttpController } from './use-cases/find-publishers/find-publishers.http.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Publisher])],
-  controllers: [PublisherController],
+  controllers: [
+    FindPublisherHttpController,
+    PublisherController
+  ],
   providers: [PublisherService]
 })
 export class PublisherModule {}

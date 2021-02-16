1721af520306d351554ef558b4c2f0abe6a7f06f
Robson Maia <robsonmvieira@gmail.com>
2021-02-16 16:34:01 -0300

    :memo: add http method response type

--- src/modules/category/use-cases/create-category/create-category.http.ts
+++ src/modules/category/use-cases/create-category/create-category.http.ts
@@ -2,8 +2,8 @@ import { routes } from "@infra/configs/app.routes";
 import CategoryService from "@modules/category/category.service";
 import CreateCategoryDto from "@modules/category/dtos/create-category.dto";
 import { Category } from "@modules/category/entities/category.entity";
-import { Body, Controller, Post } from "@nestjs/common";
-import { ApiBody } from "@nestjs/swagger";
+import { Body, Controller, HttpStatus, Post } from "@nestjs/common";
+import { ApiBody, ApiResponse } from "@nestjs/swagger";
 
 @Controller()
 export class CreateCategoryHttpController {
@@ -12,6 +12,12 @@ export class CreateCategoryHttpController {
 
   @Post(routes.category.root)
   @ApiBody({ type: CreateCategoryDto })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED
+  })
   async create(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
     return this.categoryService.create(createCategoryDto);
   }
--- src/modules/category/use-cases/find-categories/find-categories.http.controller.ts
+++ src/modules/category/use-cases/find-categories/find-categories.http.controller.ts
@@ -1,7 +1,8 @@
 import { routes } from "@infra/configs/app.routes";
 import CategoryService from "@modules/category/category.service";
 import { Category } from "@modules/category/entities/category.entity";
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpStatus } from "@nestjs/common";
+import { ApiResponse } from "@nestjs/swagger";
 
 @Controller()
 export class FindCategoriesHttpController {
@@ -9,6 +10,12 @@ export class FindCategoriesHttpController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Get(routes.category.root)
+  @ApiResponse({
+    status: HttpStatus.SERVICE_UNAVAILABLE
+  })
+  @ApiResponse({
+    status: HttpStatus.OK
+  })
   async findAll(): Promise<Category[]> {
     return this.categoryService.findAll()
   }

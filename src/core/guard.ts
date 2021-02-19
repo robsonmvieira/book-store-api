d90a4c500638baf1492b8221514bd440f7101420
Robson Maia <robsonmvieira@gmail.com>
2021-02-19 13:15:23 -0300

    :sparkles: finish guard class

--- src/core/guard.ts
+++ src/core/guard.ts
@@ -9,6 +9,42 @@ export class Guard {
       return false
     }
 
+    if(value instanceof Date) {
+      return false
+    }
+
+    if(Array.isArray(value)) {
+      if(value.length === 0) {
+        return true
+      }
+      if(value.every(item => Guard.isEmpty(item))) {
+        return true
+      }
+
+    }
+
+    if(value === '') {
+      return true
+    }
+
     return false
   }
+
+  static lengthIsBetween(
+    value: number | string | Array<unknown>,
+    min: number,
+    max: number
+  ): boolean {
+    if (Guard.isEmpty(value)) {
+      throw new Error("Cannot checked length of a value. Provide value is Empty")
+    }
+
+    const valueLength = typeof value === 'number' ? Number(value).toString().length : value.length
+
+    if(valueLength >= min && valueLength <= max) {
+      return true
+    }
+    return false
+  }
+
 }
